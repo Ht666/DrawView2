566b117c89a176952db0fb498946fdd3d69b58cc
Ht666 <1256414277@qq.com>
2015-05-23 20:26:54 +0800

    增添橡皮擦功能
    添加了橡皮擦功能，但只是通过改变画笔颜色实现。想实现，画任意大的图形，但没有实现

--- src/com/example/drawmenu3/Draw4.java
+++ src/com/example/drawmenu3/Draw4.java
@@ -31,6 +31,7 @@ public class Draw4 extends View {
 	private int View_Width;
 	private int View_height;
 	
+	
 	//创建一个缓冲区域,画布，画笔，画图路径
 	Bitmap cacheBitmap=null;
 	Canvas cachecanvas=null;
@@ -56,7 +57,7 @@ public class Draw4 extends View {
 		cacheBitmap=Bitmap.createBitmap(View_Width, View_height, Config.ARGB_8888);
 		cachecanvas=new Canvas();
 		cachecanvas.setBitmap(cacheBitmap);
-		cachecanvas.drawColor(Color.TRANSPARENT);  //画布背景颜色
+		cachecanvas.drawColor(Color.WHITE);  //画布背景颜色
 		
 		//初始化路径
 		path=new Path();
@@ -87,10 +88,10 @@ public class Draw4 extends View {
 		// 修改currentX、currentY两个属性
 		currentX=event.getX();
 		currentY=event.getY();
-		
+	
 		//根据条件实现橡皮擦功能
 		if(Eraser==true){
-			p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN)); 
+			setPaint(Color.WHITE,10);
 			switch (event.getAction()) {  
 	        case MotionEvent.ACTION_DOWN:  
 	  	        path.moveTo(currentX,currentY);  
@@ -108,9 +109,9 @@ public class Draw4 extends View {
 	            path.reset(); 
 	            break;  
 	        }  
-			
-			
+				
 		}
+		
 		// 通知当前组件重绘自己
 		invalidate();
 		// 返回true表明该处理方法已经处理该事件
@@ -126,63 +127,12 @@ public class Draw4 extends View {
 		canvas.drawBitmap(cacheBitmap, 0, 0, bmp);
 		
 	}
-
-/*	public void drawcircle(){
+	
+	//设置画笔颜色
+	public void SetColor(int Choose){
 		
 		Eraser=false;
-		cachecanvas.drawCircle(currentX, currentY,40, p);
-		//invalidate();
-	}
-	
-	public void drawline(){
-		
-		Eraser=false;
-		Path path=new Path();
-		path.moveTo(currentX, currentY);
-		path.lineTo(currentX+60,currentY +40);
-		cachecanvas.drawPath(path, p);
-	}
-	
-	public void drawtriggle(){
-		
-		Eraser=false;
-		Path path=new Path();
-		path.moveTo(currentX, currentY);
-		path.lineTo(currentX+30,currentY +40);
-		path.lineTo(currentX-50, currentY+60);
-		path.close();
-		cachecanvas.drawPath(path, p);
-	}
-	
-
-	public void drawrect(){
-		
-		Eraser=false;
-		cachecanvas.drawRect(currentX, currentY, currentX+60, currentY+40, p);
-	}
-	
-	public void drawsquare(){
-		
-		Eraser=false;
-		cachecanvas.drawRect(currentX+50, currentY, currentX, currentY+50, p);
-		//hecanvas.drawRect(currentX, currentY, currentX+50 currentY+50, p);
-	}
-	
-	public void eraser(){
-		
-		Eraser=true;
-			
-	}
-	*/
-	
-	/*public void testGithub(){
-		Log.d("GitHub", "This method just for test github system");
-	}//*/
-	
-	public void SetColor(int C){
-		
-		Eraser=false;
-		switch(C){
+		switch(Choose){
 		
 		case 1:
 			p.setColor(Color.RED);
@@ -196,30 +146,31 @@ public class Draw4 extends View {
 		}
 		
 	}
+
 	
-	public void SetDraw(int D){
+	public void SetDraw(int Choose){
 		
 		Eraser=false;
 		
-		switch(D){
+		switch(Choose){
 		
-		case 1:
+		case 4:
 			//画圆
-			cachecanvas.drawRect(currentX, currentY, currentX+60, currentY+40, p);
+			cachecanvas.drawCircle(currentX, currentY,40, p);
 			break;
-		case 3:
+		case 7:
 			//画直线
 			Path path=new Path();
 			path.moveTo(currentX, currentY);
 			path.lineTo(currentX+60,currentY +40);
 			cachecanvas.drawPath(path, p);
 			break;
-		case 2:
+		case 5:
 			//画矩形
 			cachecanvas.drawRect(currentX, currentY, currentX+60, currentY+40, p);
 			break;
-		case 4:
-			//
+		case 8:
+			//画三角形
 			Path path1=new Path();
 			path1.moveTo(currentX, currentY);
 			path1.lineTo(currentX+30,currentY +40);
@@ -227,15 +178,11 @@ public class Draw4 extends View {
 			path1.close();
 			cachecanvas.drawPath(path1, p);
 			break;
-		case 5:
+		case 6:
 			cachecanvas.drawRect(currentX+50, currentY, currentX, currentY+50, p);
 			break;
 		
 		}
-		
-		
 	}
-	
-	
 
 }
